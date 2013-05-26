e94f0306608825bfd7c991d675eac8860e3988dd
OmarBizreh <omarbizrehxda@gmail.com>
2013-05-26 10:19:37 +0200

    Initial Commit

---
+++ kernel/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 SMP PREEMPT Sat May 25 20:49:57 EET 2013"
+#define LINUX_COMPILE_BY "omar"
+#define LINUX_COMPILE_HOST "Build-Server"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
