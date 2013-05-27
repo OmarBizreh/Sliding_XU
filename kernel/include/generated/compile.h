6d58fc1e55763651fc05bd3ad1119b2f1e70b4c0
OmarBizreh <omarbizrehxda@gmail.com>
2013-05-27 19:53:12 +0200

    Add CPU Governors & IO Schedulers

--- kernel/include/generated/compile.h
+++ kernel/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 2 */
+/* This file is auto generated, version 6 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#2 SMP PREEMPT Sat May 25 20:49:57 EET 2013"
+#define UTS_VERSION "#6 SMP PREEMPT Sun May 26 15:22:04 EET 2013"
 #define LINUX_COMPILE_BY "omar"
 #define LINUX_COMPILE_HOST "Build-Server"
 #define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
--- kernel/include/generated/utsrelease.h
+++ kernel/include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.0.8_Sliding_XU-v4.0-g5cc8b65-dirty"
+#define UTS_RELEASE "3.0.8_Sliding_XU-v4.0"
